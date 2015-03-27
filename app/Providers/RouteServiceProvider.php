9740ee0cd20e858988c97e5555899f4aa1ba3d33
Frank <frank@frnk.ca>
2015-03-27 13:29:01 -0400

    Added localization library

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,8 @@ class RouteServiceProvider extends ServiceProvider {
 	{
 		parent::boot($router);
 
-		//
+		// Define global route patterns.
+        $router->pattern('lang', '[a-z]{2}');
 	}
 
 	/**
