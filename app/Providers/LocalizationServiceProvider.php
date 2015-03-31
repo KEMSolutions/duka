f5b7e82091411a67b5599c850b1628f24a9d34e9
Frank <frank@frnk.ca>
2015-03-31 12:45:22 -0400

    Committing work in progress

--- app/Providers/LocalizationServiceProvider.php
+++ app/Providers/LocalizationServiceProvider.php
@@ -1,7 +1,8 @@
 <?php namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Cache;
 use Carbon\Carbon;
+use Illuminate\Support\ServiceProvider;
 
 /**
  * Class LocalizationServiceProvider
@@ -16,7 +17,8 @@ class LocalizationServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		//
+        // Set supported locales for the Mcamara/Laravel-localization package.
+        config(['laravellocalization.supportedLocales' => $this->getSupportedLocales()]);
 	}
 
 	/**
@@ -24,36 +26,41 @@ class LocalizationServiceProvider extends ServiceProvider {
 	 *
 	 * @return void
 	 */
-	public function register()
-	{
-		// Set supported locales for Mcamara/Laravel-localization.
-        config(['laravellocalization.supportedLocales' => $this->getSupportedLocales()]);
-	}
+	public function register() {}
 
+    /**
+     * Retrieve the supported locales from KEM's API.
+     *
+     * @return mixed
+     */
     public function getSupportedLocales()
     {
-        // Retrieve the list of supported locales from the cache. If the list doesn't exist,
-        // retrieve it from KEM's API.
-//        $expiresAt  = Carbon::now()->addWeek();
-//        $locales    = $this->app['Cache']->remember('supportedlocales', $expiresAt, function()
-//        {
-//            return [
-//                'en'    => ['name' => 'English', 'script' => 'Latn', 'native' => 'English'],
-//                'en-CA' => ['name' => 'Canadian English', 'script' => 'Latn', 'native' => 'Canadian English'],
-//                'fr'    => ['name' => 'French', 'script' => 'Latn', 'native' => 'français'],
-//                'fr-CA' => ['name' => 'Canadian French', 'script' => 'Latn', 'native' => 'français canadien'],
-//            ];
-//        });
-//
-//        return $locales;
+        // Retrieve the list of supported locales from the cache. If the list
+        // doesn't exist, retrieve it from KEM's API and cache it for next time.
+        $expiresAt  = Carbon::now()->addWeek();
+        $locales    = Cache::remember('supportedlocales', $expiresAt, function()
+        {
+            // TODO: move all Guzzle code to another class and use a facade or something...
+            $client = new \GuzzleHttp\Client();
+            $data   = '' . 'hLEQPVB9OduNPC5zd3ErIRs4e1wap0Dn9SEzUXeaMyovxJbowhC6TOSY4ySRel8';
+            $sig    = base64_encode(hash('sha512', $data));
+            dd($sig);
 
+            $response = $client->get('https://kemsolutions.com/CloudServices/index.php/api/1/locales', [
+                'headers' => ['X-Kem-User' => '1', 'X-Kem-Signature' => $sig]
+            ]);
+            
+            dd($response);
+            
+            return [
+                'en'    => ['name' => 'English', 'script' => 'Latn', 'native' => 'English'],
+                'en-CA' => ['name' => 'Canadian English', 'script' => 'Latn', 'native' => 'Canadian English'],
+                'fr'    => ['name' => 'French', 'script' => 'Latn', 'native' => 'français'],
+                'fr-CA' => ['name' => 'Canadian French', 'script' => 'Latn', 'native' => 'français canadien'],
+            ];
+        });
 
-        return [
-            'en'    => ['name' => 'English', 'script' => 'Latn', 'native' => 'English'],
-            'en-CA' => ['name' => 'Canadian English', 'script' => 'Latn', 'native' => 'Canadian English'],
-            'fr'    => ['name' => 'French', 'script' => 'Latn', 'native' => 'français'],
-            'fr-CA' => ['name' => 'Canadian French', 'script' => 'Latn', 'native' => 'français canadien'],
-        ];
+        return $locales;
     }
 
 }
