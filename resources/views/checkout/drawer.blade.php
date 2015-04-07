48dcb5dedec6bcfbb3065c20bc62496cd7ad52ed
Bob <roberthang@hotmail.fr>
2015-04-07 12:53:21 -0400

    Cleared cart drawer from unused comment

--- resources/views/checkout/drawer.blade.php
+++ resources/views/checkout/drawer.blade.php
@@ -25,67 +25,11 @@
 
     <div id="cart-container" style="margin-right: 0px; display: block;">
         <div class="proceed btn-group btn-group-justified">
-            <a class="btn btn-two btn-lg" id="back">Continuer le magasinage</a>
-            <a class="btn btn-three btn-lg" id="checkout" href="#">Passer à la caisse</a>
+            <a class="btn btn-two btn-lg" id="back">{{ Lang::get("boukem.keep_shopping") }}</a>
+            <a class="btn btn-three btn-lg" id="checkout" href="{{ url("/dev/cart") }}">{{ Lang::get("boukem.check_out") }}</a>
         </div>
-            {{--<a class="btn btn-three btn-lg" id="checkout" href="/cart/index">Passer à la caisse</a>	</div>--}}
         <div id="cart-items">
             <ul class="cart-items-list">
-                {{--<li class="w-box animated bounceInDown" data-product="6484" data-quantity="1">--}}
-                    {{--<div class="col-xs-3 text-center">--}}
-                        {{--<img src="http://placehold.it/100x160" class="img-responsive">--}}
-                    {{--</div>--}}
-                    {{--<div class="col-xs-9 no-padding-left">--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="col-xs-10">--}}
-                                {{--<h3 class="product-name">Perfect probiotic - 120 g</h3>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-xs-2">--}}
-                                {{--<h4 class="text-right"><i class="fa fa-trash fa-1 close-button"><span class="sr-only">Remove from cart</span></i></h4>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="col-xs-8">--}}
-                                {{--<div class="input-group">--}}
-                                    {{--<input type="number" value="1" class="quantity form-control input-sm" min="1" step="1">--}}
-                                    {{--<span class="update_quantity_indicator input-group-addon ">--}}
-                                        {{--<i class="fa" hidden=""><span class="sr-only">Update quantity</span></i>--}}
-                                    {{--</span>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-xs-4 product-price text-right" data-price="34.99">$34.99</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</li>--}}
-
-                {{--<li class="w-box animated bounceInDown" data-product="6484" data-quantity="1">--}}
-                    {{--<div class="col-xs-3 text-center">--}}
-                        {{--<img src="http://placehold.it/100x160" class="img-responsive">--}}
-                    {{--</div>--}}
-                    {{--<div class="col-xs-9 no-padding-left">--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="col-xs-10">--}}
-                                {{--<h3 class="product-name">Perfect probiotic - 120 g</h3>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-xs-2">--}}
-                                {{--<h4 class="text-right"><i class="fa fa-trash fa-1 close-button"><span class="sr-only">Remove from cart</span></i></h4>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="col-xs-8">--}}
-                                {{--<div class="input-group">--}}
-                                    {{--<input type="number" value="1" class="quantity form-control input-sm" min="1" step="1">--}}
-                                    {{--<span class="input-group-addon update_quantity_indicator">--}}
-                                        {{--<i class="fa" hidden=""><span class="sr-only">Update quantity</span></i>--}}
-                                    {{--</span>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-xs-4 product-price text-right" data-price="34.99">$34.99</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</li>--}}
-
-
 
             </ul>
         </div>
