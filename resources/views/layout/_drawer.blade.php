cebdf01542c2f650945bff6250fe20040f224c95
hang-up <nickdanvery@hotmail.fr>
2015-05-10 19:06:13 -0400

    Fixed cart shadow when page is loaded for the first time

--- resources/views/layout/_drawer.blade.php
+++ resources/views/layout/_drawer.blade.php
@@ -1,4 +1,4 @@
-<div id="cart-container" style="margin-right: 0px; display: block;">
+<div id="cart-container" style="margin-right: 0px;">
     <div class="proceed btn-group btn-group-justified">
         <a class="btn btn-two btn-lg" id="back">{{ Lang::get("boukem.keep_shopping") }}</a>
         <a class="btn btn-three btn-lg" id="checkout" href="{{ url("/dev/cart") }}">{{ Lang::get("boukem.check_out") }}</a>
