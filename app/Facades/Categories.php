e14054e546f721970568dfe96bd3eb4f88b89be4
Frank <frank@frnk.ca>
2015-05-10 00:38:21 -0400

    Moving some methods out of KemAPI and into their own Facades

---
+++ app/Facades/Categories.php
@@ -0,0 +1,9 @@
+<?php namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Categories extends Facade {
+
+    protected static function getFacadeAccessor() { return 'App\ApiObjects\Categories'; }
+
+}
---
+++ app/Facades/Layouts.php
@@ -0,0 +1,9 @@
+<?php namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Layouts extends Facade {
+
+    protected static function getFacadeAccessor() { return 'App\ApiObjects\Layouts'; }
+
+}
