3fbcd2f76d7969dcf84b13c33998e868abdb64ec
hang-up <nickdanvery@hotmail.fr>
2015-05-13 12:57:51 -0400

    Provices and States are dynamically uploaded from json

--- public/js/boukem2.js
+++ public/js/boukem2.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
         initval: 1
     });
 
+
+    //Populate country list
     $.getJSON("/js/data/country-list.en.json", function(data) {
         var listItems = '',
             $country = $("#country");
@@ -11,5 +13,40 @@ $(document).ready(function () {
             listItems += "<option value='" + key + "'>" + val + "</option>";
         });
         $country.append(listItems);
+    });
+
+    //Populate CA provinces list
+    $.getJSON("/js/data/ca-provinces.json", function(data) {
+        var listItems = '',
+            $provinces = $("#province").find("[data-country='CA']");
+
+        $.each(data, function(key, val) {
+            listItems += "<option value='" + key + "'>" + val + "</option>";
+        });
+        $provinces.append(listItems);
+    });
+
+    //Populate US states list
+    $.getJSON("/js/data/us-states.json", function(data) {
+        var listItems = '',
+            $provinces = $("#province").find("[data-country='US']");
+
+        $.each(data, function(key, val) {
+            listItems += "<option value='" + key + "'>" + val + "</option>";
+        });
+        $provinces.append(listItems);
+    });
+
+    //Populate MX states list
+    $.getJSON("/js/data/world-states.json", function(data) {
+        var listItems = '',
+            $provinces = $("#province").find("[data-country='MX']");
+
+        $.each(data, function(key, val) {
+            if (data[key].country === "MX"){
+                listItems += "<option value='" + data[key].short + "'>" + data[key].name + "</option>";
+            }
+        });
+        $provinces.append(listItems);
     });
 });
