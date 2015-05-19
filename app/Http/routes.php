ed5a1f409758ff374b1a6da0275987846fa4473f
Frank <frank@frnk.ca>
2015-05-19 17:28:15 -0400

    Added route some names

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,14 +14,14 @@ Route::group([
 //    ]);
 
     // Homepage.
-    Route::get('/', 'LayoutController@home');
     Route::get('home', 'LayoutController@home');
+    Route::get('/', ['as' => 'home', 'uses' => 'LayoutController@home']);
 
     // Products.
-    Route::get("prod/{slug}", "ProductController@show");
+    Route::get('prod/{slug}', ['as' => 'product', 'uses' => 'ProductController@show']);
 
     // Cart & checkout.
-    Route::get("cart", "CheckoutController@index");
+    Route::get('cart', ['as' => 'cart', 'uses' => 'CheckoutController@index']);
 
     // Temporary routes, used for development.
     Route::group(['prefix' => 'dev'], function()
@@ -46,12 +46,12 @@ Route::group([
 // API endpoints.
 Route::group(['prefix' => 'api', 'middleware' => 'csrf.any'], function()
 {
-    Route::get('brands/{id}',     'ApiController@getBrand');
-    Route::get('categories/{id}', 'ApiController@getCategory');
-    Route::get('layouts/{id?}',   'ApiController@getLayout');
-    Route::get('products/{id}',   'ApiController@getProduct');
-    Route::get('search/{query}',  'ApiController@searchProducts');
-    Route::post('estimate',       'ApiController@getOrderEstimate');
+    Route::get('brands/{id}',     ['as' => 'api.brands', 'uses' => 'ApiController@getBrand']);
+    Route::get('categories/{id}', ['as' => 'api.categories', 'uses' => 'ApiController@getCategory']);
+    Route::get('layouts/{id?}',   ['as' => 'api.layouts', 'uses' => 'ApiController@getLayout']);
+    Route::get('products/{id}',   ['as' => 'api.products', 'uses' => 'ApiController@getProduct']);
+    Route::get('search/{query}',  ['as' => 'api.search', 'uses' => 'ApiController@searchProducts']);
+    Route::post('estimate',       ['as' => 'api.estimate', 'uses' => 'ApiController@getOrderEstimate']);
 
     Route::any('/{catchAll}', function($catchAll) {
         return Illuminate\Http\JsonResponse::create(['status' => 400, 'error' => 'Bad request.'], 400);
