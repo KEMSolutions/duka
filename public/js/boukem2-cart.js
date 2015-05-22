cbdd2982d0ea7978cd695dceca90c664b4d70d6c
Bob <roberthang@hotmail.fr>
2015-05-22 16:01:01 -0400

    Updated documentation

--- public/js/boukem2-cart.js
+++ public/js/boukem2-cart.js
@@ -164,6 +164,13 @@ var estimateContainer = {
         });
     },
 
+    /**
+     * Get the relevant taxes according to the chosen shipping method.
+     *
+     * @param serviceCode
+     * @param data
+     * @returns {string}
+     */
     getShipmentTaxes : function(serviceCode, data) {
         var taxes = 0;
 
@@ -203,8 +210,6 @@ var estimateContainer = {
         var postcode_value = $("#postcode").val();
         var country_value = $("#country").val();
 
-        console.log(data);
-
         for(var i = 0; i<data.shipping.services.length; i++)
         {
             var serviceDOM = "<tr data-service='" + data.shipping.services[i].service_code + "'>" +
@@ -235,25 +240,43 @@ var estimateContainer = {
 
 }
 
+/**
+ * Object responsible for handling the payment panel.
+ *
+ * @type {{displayPaymentPanel: Function, initPaymentPanel: Function, updatePaymentPanel: Function, getTaxes: Function, init: Function}}
+ */
 var paymentContainer = {
+    /**
+     * Displays the Payment panel.
+     *
+     */
     displayPaymentPanel : function() {
         $("#payment").removeClass("hidden").addClass("animated fadeInDown");
         $("#checkoutButton").addClass("animated rubberBand");
     },
 
+    /**
+     * Populate the payment panel with default values.
+     *
+     * @param data
+     */
     initPaymentPanel : function(data) {
         var subtotal = parseFloat(UtilityContainer.getProductsPriceFromSessionStorage()).toFixed(2),
             priceTransport = $("input:radio.shipping_method:checked").data("cost"),
             taxes = paymentContainer.getTaxes(data) + parseFloat($("input:radio.shipping_method:checked").data("taxes")),
             total = parseFloat(subtotal) + parseFloat(priceTransport) + parseFloat(taxes);
 
-        console.log(priceTransport);
         $("#price_subtotal").text(subtotal);
         $("#price_transport").text(priceTransport);
         $("#price_taxes").text(taxes);
         $("#price_total").text(total);
     },
 
+    /**
+     * Update the payment panel with right values (shipping method)
+     *
+     * @param data
+     */
     updatePaymentPanel : function(data) {
         var subtotal = parseFloat(UtilityContainer.getProductsPriceFromSessionStorage()).toFixed(2),
             priceTransport, taxes;
@@ -272,6 +295,12 @@ var paymentContainer = {
 
     },
 
+    /**
+     * Get the total taxes (TPS/TVQ or TVH or TPS or null) + shipping method taxes.
+     *
+     * @param data
+     * @returns {number}
+     */
     getTaxes : function(data) {
         var taxes = 0;
 
@@ -286,6 +315,11 @@ var paymentContainer = {
         return taxes;
     },
 
+    /**
+     * Register methods for outside calling.
+     *
+     * @param data
+     */
     init : function(data) {
         paymentContainer.displayPaymentPanel();
         paymentContainer.initPaymentPanel(data);
