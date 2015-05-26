fd389634eb9e00dbd3a3ce7f846d529d55f6fa15
Frank <frank@frnk.ca>
2015-05-26 15:01:17 -0400

    Added paramters to some requests + prep for orders request

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,57 +11,74 @@ use Request;
 use Illuminate\Http\JsonResponse;
 
 /**
- * Class ApiController. CSRF token is checked automatically.
+ * This class handles all API requests through Boukem 2, e.g. jQuery AJAX requests.
+ * The CSRF token is checked automatically.
  *
  * @package App\Http\Controllers
  */
 class ApiController extends Controller
 {
     /**
-     * @param $id
-     * @return mixed
+     * Retrieves the details for a brand.
+     * See App\ApiObjects\Brands
      */
-    public function getBrand($id) {
-        return $this->send(Brands::get($id));
+    public function getBrand($id)
+    {
+        return $this->send(Brands::get($id,
+            Request::input('page', 1),
+            Request::input('perPage', 40)
+        ));
     }
 
     /**
-     * @param $id
-     * @return mixed
+     * Retrieves the details for a category.
+     * See App\ApiObjects\Categories
      */
-    public function getCategory($id) {
-        return $this->send(Categories::get($id));
+    public function getCategory($id)
+    {
+        return $this->send(Categories::get($id,
+            Request::input('page', 1),
+            Request::input('perPage', 40)
+        ));
     }
 
     /**
-     * @param string $id
-     * @return mixed
+     * Retrieves the layouts for a page.
+     * See App\ApiObjects\KemApiObject
      */
     public function getLayout($id = '') {
         return $this->send(Layouts::get($id));
     }
 
     /**
-     * @param $id
-     * @return static
+     * Retrieves shipping costs and delivery time estimates.
+     * See App\ApiObjects\Orders
+     */
+    public function getOrderEstimate()
+    {
+        return $this->send(Orders::estimate(
+            (array) Request::input('products'),
+            (array) Request::input('shipping_address')
+        ));
+    }
+
+    /**
+     * Retrieves the details for a product.
+     * See App\ApiObjects\KemApiObject
      */
     public function getProduct($id) {
         return $this->send(Products::get($id));
     }
 
     /**
-     * @param $query
-     * @return static
+     * Retrieves search results for a query.
+     * See App\ApiObjects\Products
      */
-    public function searchProducts($query) {
-        return $this->send(Products::search($query));
-    }
-
-    public function getOrderEstimate()
+    public function searchProducts($query)
     {
-        return $this->send(Orders::estimate(
-            (array) Request::input('products'),
-            (array) Request::input('shipping_address')
+        return $this->send(Products::search($query,
+            Request::input('page', 1),
+            Request::input('perPage', 40)
         ));
     }
 
