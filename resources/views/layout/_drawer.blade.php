50fea6b996be9e164240c2948b4e861c076208cf
Bob <roberthang@hotmail.fr>
2015-06-01 17:31:52 -0400

    Updated old flabby cart drawer to a new kick ass one (products scroll to fix, overlay to implement)

--- resources/views/layout/_drawer.blade.php
+++ resources/views/layout/_drawer.blade.php
@@ -1,13 +1,46 @@
-<div id="cart-container" style="margin-right: 0px;">
-    <div class="proceed btn-group btn-group-justified">
-        <a class="btn btn-two btn-lg" id="back">{{ Lang::get("boukem.keep_shopping") }}</a>
-        <a class="btn btn-three btn-lg" id="checkout" href="{{ url("/dev/cart") }}">{{ Lang::get("boukem.check_out") }}</a>
+<aside id="cart-container" style="margin-right: 0">
+    <div class="cart">
+        <header class="cart-header">
+            <i class="fa fa-angle-left fa-2x" id="back"></i>
+            <h2>Your cart</h2>
+        </header>
+
+        <div id="cart-items">
+            <ul class="cart-items-list">
+
+            </ul>
+        </div>
+
+        <div class="cart-footer">
+            <div class="cart-price order-summary">
+                <div class="cart-subtotal">
+                    <dl class="calculation">
+                        <dt>Subtotal</dt>
+                        <dd>$85.00</dd>
+                    </dl>
+
+                    <dl class="calculation">
+                        <dt>Taxes</dt>
+                        <dd>$7.66</dd>
+                    </dl>
+
+                    <dl class="calculation">
+                        <dt>Shipping</dt>
+                        <dd>FREE</dd>
+                    </dl>
+
+                </div>
+                <div class="cart-total"><dl class="calculation total">
+                        <dt>Total</dt>
+                        <dd>$92.66</dd>
+                    </dl></div>
+            </div>
+            <div class="cart-actions">
+                <a class="checkout" id="checkout" href="{{ url("/dev/cart") }}">
+                    <span class="cart-action-text">{{ Lang::get("boukem.check_out") }}</span>
+                </a>
+            </div>
+        </div>
     </div>
-    <div id="cart-items">
-        <ul class="cart-items-list">
 
-        </ul>
-    </div>
-</div>
-
-
+</aside>
