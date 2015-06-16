f83a57a797cb64a7cb33cf107e45ba1308fcdecc
Frank <frank@frnk.ca>
2015-06-16 17:17:20 -0400

    Added get method to retrieve order details while witholding sensitive information.

--- app/ApiObjects/Orders.php
+++ app/ApiObjects/Orders.php
@@ -2,6 +2,8 @@
 
 use App\Facades\Utilities;
 use Log;
+use Auth;
+use Lang;
 use Cache;
 use KemAPI;
 use Carbon\Carbon;
@@ -9,6 +11,26 @@ use Carbon\Carbon;
 class Orders extends KemApiObject
 {
     public function __construct() { parent::__construct('orders'); }
+
+    public function get($id, $verification = null)
+    {
+        // Retrieve order details.
+        $original = parent::get($id);
+
+        // Check that user can view the order details.
+        if (!Auth::check() && $verification != $original->verification) {
+            abort(404, Lang::get('boukem.no_exist'));
+        }
+
+        // Remove sensitive information.
+        $order = new \stdClass;
+        $order->id = $original->id;
+        $order->status = $original->status;
+        $order->payment_details = new \stdClass;
+        $order->payment_details->payment_url = $original->payment_details->payment_url;
+
+        return $order;
+    }
 
     /**
      * Retrieves shipping costs and delivery time estimates.
@@ -97,7 +119,9 @@ class Orders extends KemApiObject
             $data->billing_address = $billingAddress;
         }
 
+//        dd($data);
         $response = KemAPI::post($this->baseRequest, $data);
+//        dd($response);
 
         // Check that response is not an error
         if (property_exists($response, 'error'))
