2571d71e7f3fedbad9d4d5c5e27403f53e8d9d24
hang-up <nickdanvery@hotmail.fr>
2015-06-16 12:37:12 -0400

    Harmonized regular featured layout

--- resources/views/component/_product_card.blade.php
+++ resources/views/component/_product_card.blade.php
@@ -1,21 +1,21 @@
 @foreach($products as $product)
       {{--TODO: TEMPORARY FIX AS TO WHY THE $products array contains empty fields?--}}
       {{--{{ dd($products) }}--}}
-    @if(is_object($product))
+    @if(is_object($product) && isset($product->brand->slug))
         <div class="item col-lg-3 col-md-4 col-sm-6">
             <div class="w-box">
                 <figure>
 
-                    {{--TODO: SET $product->slug instead of ID when facade is ready. --}}
-                    <a href="/{{ $locale }}/dev/prod/{{ $product->id }}" class="strong">
+                    <a href="/{{ $locale }}/dev/prod/{{ $product->slug }}" class="strong">
                         <img src="//static.boutiquekem.com/productimg-300-280-{{ $product->images[0]->id . "." . $product->images[0]->extension }}" class="img-responsive center-block "/>
                     </a>
 
-                    {{--<a href="/{{ $locale }}/prod/{{ $product->slug }}">--}}
-                        {{--<img src="//static.boutiquekem.com/productimg-300-280-{{ $product->images[0]->id . "." . $product->images[0]->extension }}" class="img-responsive center-block "/>--}}
-                    {{--</a>--}}
+                    <span class="date-over">
+                        <strong>
+                            <a class="color-two" href="{{ $locale }}/cat/{{ $product->brand->slug }}">{{ $product->brand->name }}</a>
+                        </strong>
+                    </span>
 
-                    <span class="date-over"><strong><a class="color-two" href="{{ $locale }}/cat/{{ $product->brand->slug }}">{{ $product->brand->name }}</a></strong></span>
                     <h2><a href="/{{ $locale }}/prod/{{ $product->slug }}" class="color-two">{{ $product->localization->name }}</a></h2>
 
                     <p>
@@ -24,7 +24,7 @@
 
 
                 <span class="w-footer">
-                    <div class="pull-left"><strong class="pricetag">{{ $product->price }} $</strong></div>
+                    <div class="pull-left"><strong class="pricetag">$ {{ number_format((float)$product->price, 2, '.', '') }}</strong></div>
 					<button class="btn btn-success pull-right buybutton"
                             data-product="{{ $product->id }}"
                             data-price="{{ $product->price }}"
@@ -32,7 +32,7 @@
                             data-thumbnail_lg="//static.boutiquekem.com/productimg-70-110-{{ count($product->images) > 0 ? $product->images[0]->id . "." . $product->images[0]->extension : "0000.png" }}"
                             data-name="{{ $product->localization->name }}"
                             data-quantity="1">
-                        ><i class="fa fa-shopping-cart"></i> {{ Lang::get("boukem.buy") }}</button>
+                        <i class="fa fa-shopping-cart"></i> {{ Lang::get("boukem.buy") }}</button>
                     <span class="clearfix"></span>
 
                 </span>
--- resources/views/site/_featured.blade.php
+++ resources/views/site/_featured.blade.php
@@ -9,7 +9,7 @@
 
                 {{--Include the appropriate layout (dense or regular cards) according to the layoutDense boolean--}}
                 @if($layoutData["featured"]["layoutDense"])
-                    @include("component._product_card_dense",
+                    @include("component._product_card",
                     ["products" => array_slice($layoutData["featured"]["products"], 0, $layoutData["featured"]["limit"])])
                 @elseif(!$layoutData["featured"]["layoutDense"])
                     @include("component._product_card",
