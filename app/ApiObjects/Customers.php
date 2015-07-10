75e8b41449ff5e4dab39d6457c15977c80cc08f0
Frank <frank@frnk.ca>
2015-07-10 17:15:40 -0400

    Added PUT method to Customers API + added comments

--- app/ApiObjects/Customers.php
+++ app/ApiObjects/Customers.php
@@ -1,14 +1,42 @@
 <?php namespace App\ApiObjects;
 
 use KemAPI;
+use Localization;
 
 class Customers extends KemApiObject
 {
+    /**
+     * @var string  Validating base64 encoded emails.
+     */
+    protected $slugInvalidCharacters = '/[^a-z0-9=\/+]/i';
+
     public function __construct() { parent::__construct('customers'); }
 
     public function create($email, $name = null, $postcode = null, $language = null)
     {
-        // TODO: validate data (using User class?).
+        $user = $this->getCustomerObject($email, $name, $postcode, $language);
+
+        // TODO: check for validation errors.
+        // ...
+
+        // Create user.
+        return KemAPI::post($this->baseRequest, $user);
+    }
+
+    public function update($id, $email, $name = null, $postcode = null, $language = null)
+    {
+        $user = $this->getCustomerObject($email, $name, $postcode, $language);
+
+        // TODO: check for validation errors.
+        // ...
+
+        // Create user.
+        return KemAPI::put($this->baseRequest .'/'. $id, $user);
+    }
+
+    private function getCustomerObject($email, $name = null, $postcode = null, $language = null)
+    {
+        // TODO: validate data.
         // ...
 
         // Build user object for API.
@@ -16,9 +44,8 @@ class Customers extends KemApiObject
         $user->email = $email;
         $user->name = $name;
         $user->postcode = $postcode;
-        $user->language = $language ?: \Localization::getCurrentLocale();
+        $user->language = $language ?: Localization::getCurrentLocale();
 
-        // Create user.
-        return KemAPI::post($this->baseRequest, $user);
+        return $user;
     }
 }
