c962355929109c9928597394fbdddd81f5614535
frnkly <frank@frnk.ca>
2015-07-17 15:53:49 -0400

    Updates to filters parameter validation for API requests

--- app/Utilities/Utilities.php
+++ app/Utilities/Utilities.php
@@ -1,19 +1,22 @@
 <?php namespace App\Utilities;
 
+use Localization;
+
 class Utilities
 {
     public function __construct()
     {
         // Build the cache namespace.
-        $this->cacheNamespace = \Localization::getCurrentLocale() .'.utilities.';
+        $this->cacheNamespace = Localization::getCurrentLocale() .'.utilities.';
     }
 
     /**
      * Returns a list of countries, sorted by country code.
      *
-     * @return array Countries, sorted by code.
+     * @return array    Countries, sorted by code.
      */
     public function getCountryList() {
-        return include __DIR__ .'/Countries/'. \Localization::getCurrentLocale() .'.php';
+        return include __DIR__ .'/Countries/'. Localization::getCurrentLocale() .'.php';
     }
 }
+
