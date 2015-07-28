408895aba89bb35efe3fcfef6ce22f5a1990732b
frnkly <frank@frnk.ca>
2015-07-28 12:40:59 -0400

    Modified to retrieve supported locales from Store

--- app/ApiObjects/Store.php
+++ app/ApiObjects/Store.php
@@ -1,5 +1,10 @@
 <?php namespace App\ApiObjects;
 
+use Cache;
+use KemAPI;
+
+use Carbon\Carbon;
+
 class Store extends BaseObject
 {
     public function __construct() { parent::__construct('store'); }
@@ -11,6 +16,27 @@ class Store extends BaseObject
      */
     public function info() {
         return parent::get('');
+    }
+
+    public function locales()
+    {
+        $locales = Cache::remember('store.locales', Carbon::now()->addDay(), function()
+        {
+            $results = KemAPI::get('store', ['embed' => 'locales']);
+
+            // Format results for use with Mcamara\LaravelLocalization.
+            foreach ($results->locales as $loc) {
+                $locales[$loc->language] = [
+                    'name' => $loc->language_name,
+                    'native' => $loc->language_name,
+                    'script' => $loc->script
+                ];
+            }
+
+            return $locales;
+        });
+
+        return $locales;
     }
 
     public function logo($width = 200, $height = 60, $mode = 'fit')
