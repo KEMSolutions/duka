5ef3a61f7c9d71f4e680a0ff04372c71ae265ef5
frnkly <frank@frnk.ca>
2015-08-04 12:58:47 -0400

    Updated get method to match parent

--- app/ApiObjects/Brands.php
+++ app/ApiObjects/Brands.php
@@ -5,22 +5,6 @@ class Brands extends BaseObject
     public function __construct() { parent::__construct('brands'); }
 
     /**
-     * Retrieves the details for a brand.
-     *
-     * @param mixed $id     ID or slug of the brand.
-     * @param int $page     The page to start from (see: https://developer.github.com/v3/#pagination).
-     * @param int $perPage  The number of products to display per page (see: https://developer.github.com/v3/#pagination).
-     * @return object       Brand object.
-     */
-    public function get($id, $page = 1, $perPage = 40) {
-        return parent::get($id, [
-            'embed' => 'products',
-            'page' => $page,
-            'per_page' => $perPage
-        ]);
-    }
-
-    /**
      * Retrieves a nested list of brands.
      *
      * @return array    List of categories.
