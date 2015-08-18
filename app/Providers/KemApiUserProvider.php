0f4352d0d738f7b26af14f7edf5dce23752c28a4
frnkly <frank@frnk.ca>
2015-08-18 17:46:57 -0400

    Creating base classes for custom KemApiUserProvider

---
+++ app/Providers/KemApiUserProvider.php
@@ -0,0 +1,69 @@
+<?php namespace App\Providers;
+
+use Customers;
+
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+
+class KemApiUserProvider implements UserProvider
+{
+    /**
+     * The hasher implementation.
+     *
+     * @var \Illuminate\Contracts\Hashing\Hasher
+     */
+    protected $hasher;
+
+    /**
+     * @param  \Illuminate\Contracts\Hashing\Hasher  $hasher
+     */
+    public function __construct(HasherContract $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier)
+    {
+        return Customers::get($identifier);
+    }
+
+    /**
+     * Retrieve a user by their unique identifier and "remember me" token.
+     *
+     * @param  mixed  $identifier
+     * @param  string  $token
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByToken($identifier, $token)
+    {
+        $customer = $this->retrieveById($identifier);
+
+        // TODO
+
+        return null;
+    }
+
+    public function updateRememberToken(UserContract $user, $token) {}
+
+    public function retrieveByCredentials(array $credentials)
+    {
+        // TODO
+
+        return null;
+    }
+
+    public function validateCredentials(UserContract $user, array $credentials)
+    {
+        $plain = $credentials['password'];
+
+        return $this->hasher->check($plain, $user->getAuthPassword());
+    }
+
+}
