aff5714f56b02f3f75f7eefc3cda0c82d3fcd382
frnkly <frank@frnk.ca>
2015-08-25 14:30:11 -0400

    Removed postal code field while brainstorming how to edit addresses

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -43,13 +43,6 @@
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Postal/ZIP Code</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="postcode" value="{{ $user->postcode }}" required>
-							</div>
-						</div>
-
-						<div class="form-group">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ $user->email }}" required>
