f65c5bf3eca31e7fcb549b4425fffc013ded299b
robhang <robert@kemsolutions.com>
2015-08-27 17:24:40 -0400

    Change class order

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="color-four">
-    <div class="ui container stackable vertically padded grid">
+    <div class="ui grid container stackable vertically padded">
         <div class="row">
 
             {{-- Contract pages --}}
