599d6c86c2bb76b2d90d11fd3ee5d2fec2f3efa8
robhang <robert@kemsolutions.com>
2015-08-27 17:20:48 -0400

    Footer turned cement-ic (semantic, got it?)

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -1,85 +1,84 @@
 <footer class="color-four">
-    <div class="container">
-
+    <div class="ui container vertically padded grid">
         <div class="row">
 
             {{-- Contract pages --}}
-            <div class="col-md-4">
-                <div class="col">
-                    <h4>{{ Lang::get("boukem.information") }}</h4>
-                    <ul>
-                        @foreach (Store::contracts() as $contract)
-                            <li>
-                                <a href="{{ route('contract', ['slug' => $contract->slug]) }}">
-                                    {{ $contract->title }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
+            <div class="five wide column">
+                <h2 class="ui header white">
+                    @lang("boukem.information")
+                </h2>
 
-            {{-- Quick links --}}
-            <div class="col-md-4">
-                <div class="col">
-                    <h4>{{ Lang::get("boukem.shortcuts") }}</h4>
-                    <ul>
+                <ul>
+                    @foreach (Store::contracts() as $contract)
                         <li>
-                            <a href="{{ route('home') }}">
-                                {{ Lang::get("boukem.home") }}
+                            <a href="{{ route('contract', ['slug' => $contract->slug]) }}">
+                                {{ $contract->title }}
                             </a>
                         </li>
-                        {{--TODO : display category links ($category_links_html)--}}
-                    </ul>
-                </div>
+                    @endforeach
+                </ul>
             </div>
 
-            {{-- User links --}}
-            <div class="col-md-4">
-                <div class="col">
-                    <h4>{{ Lang::get("boukem.customer_service") }}</h4>
-                    <ul>
 
-                        @if (Auth::guest())
+            {{-- Quick links --}}
+            <div class="five wide column">
+                <h2 class="ui header white">
+                    @lang("boukem.shortcuts")
+                </h2>
 
-                            <li>
-                                <a href="{{ route('auth.login') }}">
-                                    {{ Lang::get("boukem.account") }}
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('auth.register')  }}">
-                                    {{ Lang::get("boukem.sign_up") }}
-                                </a>
-                            </li>
-
-                        @else
-
-                            <li>
-                                <a href="{{ route('auth.account') }}">
-                                    {{ Lang::get("boukem.account") }}
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('auth.logout') }}">
-                                    @lang("boukem.log_out")
-                                </a>
-                            </li>
-
-                        @endif
-                    </ul>
-                </div>
+                <ul>
+                    <li>
+                        <a href="{{ route('home') }}">
+                            {{ Lang::get("boukem.home") }}
+                        </a>
+                    </li>
+                </ul>
             </div>
 
+
+            {{--User links--}}
+            <div class="five wide column">
+                <h2 class="ui header white">
+                    @lang("boukem.customer_service")
+                </h2>
+
+                <ul>
+                    @if(Auth::guest())
+                        <li>
+                            <a href="{{ route('auth.login') }}">
+                                {{ Lang::get("boukem.account") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('auth.register')  }}">
+                                {{ Lang::get("boukem.sign_up") }}
+                            </a>
+                        </li>
+                    @else
+                        <li>
+                            <a href="{{ route('auth.account') }}">
+                                {{ Lang::get("boukem.account") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('auth.logout') }}">
+                                @lang("boukem.log_out")
+                            </a>
+                        </li>
+                    @endif
+                </ul>
+            </div>
         </div>
 
+        <div class="ui divider"></div>
+
         {{-- Copyright & Powered By --}}
-        <hr />
         <div class="row">
-            <div class="col-lg-9 copyright">
+            <div class="left floated three wide column white">
                 &copy; <?php echo date("Y"); ?>, {{ Lang::get("boukem.copyrights") }}.
             </div>
-            <div class="col-lg-3 footer-logo text-right">
+
+            <div class="right floated three wide column white">
                 <img src="{{ asset('/img/kem_signature.png') }}" alt="Powered by KEM">
             </div>
         </div>
