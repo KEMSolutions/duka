8de60973164e8656b165d834157dbfae1de6a1eb
robhang <robert@kemsolutions.com>
2015-09-03 12:56:19 -0400

    filters summary converted

--- resources/views/site/category/_filter_summary.blade.php
+++ resources/views/site/category/_filter_summary.blade.php
@@ -1,12 +1,22 @@
-<div class="col-md-10 border-bottom">
-    <h4>{{ Lang::get("boukem.filter_by") }}</h4>
-    <button class="btn btn-black btn-xs pull-right">{{ Lang::get("boukem.reset") }}</button>
+<div class="twelve wide column">
+    <div class="row border-bottom-clear">
+        <h4 class="ui header">{{ Lang::get("boukem.filter_by") }}</h4>
 
-    <ul class="list-unstyled list-inline no-margin-bottom">
-        <li><a class="ui orange tag label">Filter 1</a></li>
-        <li><a class="ui teal tag label">Filter 2</a></li>
-        <li><a class="ui black tag label">Filter 3</a></li>
-    </ul>
+        <button class="btn btn-black btn-xs pull-right">{{ Lang::get("boukem.reset") }}</button>
+
+        <div class="ui horizontal list">
+            <div class="item">
+                <a class="ui orange tag label">Filter 1</a>
+            </div>
+
+            <div class="item">
+                <a class="ui teal tag label">Filter 2</a>
+            </div>
+
+            <div class="item">
+                <a class="ui black tag label">Filter 3</a>
+            </div>
+        </div>
+    </div>
+
 </div>
-
-
--- resources/views/site/category/index.blade.php
+++ resources/views/site/category/index.blade.php
@@ -17,8 +17,8 @@
                 {{-- Include filter sidebar --}}
                 @include('site.category._filter_sidebar', ['total' => $total])
 
-                {{-- Include filter summary container --}}
-                {{--@include('site.category._filter_summary')--}}
+                 {{--Include filter summary container--}}
+                @include('site.category._filter_summary')
 
                 {{-- Include sorting topbar --}}
                 {{--@include('site.category._sorting_topbar')--}}
