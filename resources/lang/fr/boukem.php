ddd6fbceac17c4fad21c0053f08f315867f39974
robhang <robert@kemsolutions.com>
2015-09-13 13:45:28 -0400

    minor typo

--- resources/lang/fr/boukem.php
+++ resources/lang/fr/boukem.php
@@ -211,7 +211,7 @@ return [
     "wishlist"          =>"Votre liste d'envies",
     "wishlist_has"      =>"Votre liste d'envies a",
     "wishlist_number"   =>"Vous avez <span id='quantity-wishlist'>0 item</span> dans votre liste d'envies.",
-    "wishlist_saved"    =>"Votre liste d'envies sera sauvegardée tant que votre historique ne sera pas effacée.",
+    "wishlist_saved"    =>"Votre liste d'envies sera sauvegardée tant que votre historique ne sera pas effacé",
     "wishlist_remove"   =>"Retirer de la liste",
     "payment_successful" => "Merci pour votre paiement.",
     "results_found"     =>":total résultats trouvés",
