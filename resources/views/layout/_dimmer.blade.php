83330e0df3423816adfc0bdc73c275356b8c6198
Robert <nickdanvery@gmail.com>
2015-11-09 21:17:33 -0500

    Added blur filter to cart dimmer.

--- resources/views/layout/_dimmer.blade.php
+++ resources/views/layout/_dimmer.blade.php
@@ -1,8 +1,9 @@
 <div class="ui inverted page dimmer cart-dimmer">
-    <button class="ui red button close-cart-dimmer one-half pull-left no-margin">@lang("boukem.close_cart")</button>
-    <button class="ui green button close-cart-dimmer one-half pull-right no-margin">@lang("boukem.checkout")</button>
+    <button class="ui btn close-cart-dimmer one-half pull-left no-margin">@lang("boukem.close_cart")</button>
+    <button class="ui btn btn-one btn-one-inverted close-cart-dimmer one-half pull-right no-margin">@lang("boukem.checkout")</button>
 
     <hr/>
+
     <div class="ui container">
         <div class="content">
             <div class="ui divided items cart-items-list dimmered"></div>
