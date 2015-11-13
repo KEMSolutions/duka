32f2936806be33aa72d7994e7955a99360d2f9dd
Robert <nickdanvery@gmail.com>
2015-11-13 13:48:35 -0500

    Re-indented last commit code

--- resources/views/site/search/_results.blade.php
+++ resources/views/site/search/_results.blade.php
@@ -1,43 +1,43 @@
-        <div class="ui stackable grid layout-toggle-container grid-layout">
-            @if (isset($results->tags) && count($results->tags))
-                <div class="ui container grid vertically padded">
-                    <div class="row">
-                        <div class="column" style="padding-top: 2rem">
-                            <h3 class="ui header">@lang("boukem.handpick")</h3>
-                        </div>
-                    </div>
-
-                    @include(
-                    'product.layout._product_card_dense', [
-                    'showTag' => false,
-                    'locale' => Localization::getCurrentLocale(),
-                    'products' => $results->tags[0]->products
-                ])
-
-                </div>
-                <hr style="width: 100%"/>
-            @endif
-
-
-            <div class="ui container grid vertically padded">
-                <div class="row">
-                    <div class="column" style="padding-top: 2rem">
-                        <h3 class="ui header">@lang('boukem.results_for', ["displayed" => $displayed, "total" => $total, "term" => $query])</h3>
-                    </div>
+<div class="ui stackable grid layout-toggle-container grid-layout">
+    @if (isset($results->tags) && count($results->tags))
+        <div class="ui container grid vertically padded">
+            <div class="row">
+                <div class="column" style="padding-top: 2rem">
+                    <h3 class="ui header">@lang("boukem.handpick")</h3>
                 </div>
             </div>
 
-
             @include(
             'product.layout._product_card_dense', [
-                'showTag' => false,
-                'locale' => Localization::getCurrentLocale(),
-                'border' => false,
-                'products' => $results->organic_results
+            'showTag' => false,
+            'locale' => Localization::getCurrentLocale(),
+            'products' => $results->tags[0]->products
         ])
-                <div class="row" style="text-align: center; display: block">
-                    {!! $paginator->render() !!}
-                </div>
 
         </div>
+        <hr style="width: 100%"/>
+    @endif
+
+
+    <div class="ui container grid vertically padded">
+        <div class="row">
+            <div class="column" style="padding-top: 2rem">
+                <h3 class="ui header">@lang('boukem.results_for', ["displayed" => $displayed, "total" => $total, "term" => $query])</h3>
+            </div>
+        </div>
+    </div>
+
+
+    @include(
+    'product.layout._product_card_dense', [
+        'showTag' => false,
+        'locale' => Localization::getCurrentLocale(),
+        'border' => false,
+        'products' => $results->organic_results
+])
+    <div class="row" style="text-align: center; display: block">
+        {!! $paginator->render() !!}
+    </div>
+
+</div>
 
