42682b8bdab5090b16a66ccf6be2925da5b036d5
Remy Vanherweghem <remy@Remys-MacBook-Pro.local>
2015-11-20 14:37:22 -0500

    Added the route to localizations and endpoints in a meta tag so we avoid hardcoding the path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}"/>
 
+	<meta name="duka-localizations-and-endpoints-url" content="{{ route('localizationsAndEndpoints') }}"/>
+
 	@if(Auth::check())
 		<meta name="user-login" content="{{ Auth::user()->id}}"/>
 	@endif
