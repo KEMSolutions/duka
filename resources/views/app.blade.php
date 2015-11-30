5851323646e46b13c287a4f446362617270d8a9a
Remy Vanherweghem <remy@Remys-MBP.sf.umontreal.ca>
2015-11-30 18:09:32 -0500

    Added a custom_metas directive to the page heads

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}"/>
-
 	<meta name="duka-localizations-and-endpoints-url" content="{{ route('localizationsAndEndpoints') }}"/>
+	@yield("custom_metas")
 
 	@if(Auth::check())
 		<meta name="user-login" content="{{ Auth::user()->id}}"/>
