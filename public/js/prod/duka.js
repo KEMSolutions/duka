cc2a436beafa639f62d12bf1f1a577da55829ed6
Robert <nickdanvery@gmail.com>
2016-02-15 12:35:35 -0500

    Added float limitation to subtotal utility function.

--- public/js/prod/duka.js
+++ public/js/prod/duka.js
@@ -119,7 +119,7 @@ var UtilityContainer = {
             total += (products[i].price * products[i].quantity);
         }
 
-        return total;
+        return total.toFixed(2);
     },
 
     /**
@@ -1790,6 +1790,121 @@ var responsiveContainer = {
     }
 }
 /**
+ * Component responsible for activating semantic ui features.
+ *
+ * @type {{module: {initDropdownModule: Function, initRatingModule: Function, initPopupModule: Function, initCheckboxModule: Function}, behaviors: {closeDimmer: Function}, init: Function}}
+ */
+var semanticInitContainer = {
+
+    /**
+     * Initialize modules
+     *
+     */
+    module: {
+        /**
+         * Initialize dropdown module.
+         *
+         */
+        initDropdownModule: function() {
+            $(".ui.dropdown").dropdown();
+
+            $(".ui.dropdown").on("click", function () {
+                var action = $(this).data("action") || "activate";
+
+                $(this).dropdown({
+                    action: action
+                });
+            });
+        },
+
+        /**
+         * Initialize rating module.
+         *
+         */
+        initRatingModule: function () {
+            $(".ui.rating").rating();
+        },
+
+        /**
+         * Initialize popup module.
+         *
+         */
+        initPopupModule: function () {
+            $(".popup").popup();
+        },
+
+        /**
+         * Initialize checkbox module.
+         *
+         */
+        initCheckboxModule: function () {
+            $('.ui.checkbox')
+                .checkbox()
+            ;
+        },
+
+        /**
+         * Initialize accordion module.
+         *
+         */
+        initAccordionModule: function() {
+            $('.ui.accordion').accordion();
+        }
+    },
+
+    /**
+     * Specify semantic custom behavior.
+     *
+     */
+    behaviors: {
+        closeDimmer: function () {
+            $(".close-dimmer").on("click", function() {
+                $(".dimmer").dimmer("hide");
+            });
+        }
+    },
+
+    /**
+     * Specify custom form validation rules.
+     *
+     */
+    rules: {
+        postalCode: function() {
+            $.fn.form.settings.rules.postalCode = function(value, fieldIdentifier) {
+                if(document.getElementById('checkboxSuccess').checked && fieldIdentifier == "billingCountry") {
+                    return true;
+                } else {
+                    if ($("#" + fieldIdentifier).val() === "CA")
+                        return value.match(/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} ?\d{1}[A-Z]{1}\d{1}$/i) ? true : false;
+                    else if ($("#" + fieldIdentifier).val() === "US")
+                        return value.match(/^\d{5}(?:[-\s]\d{4})?$/) ? true : false;
+                    else {
+                        return true;
+                    }
+                }
+            }
+        }
+    },
+
+
+    init: function () {
+        var self = semanticInitContainer,
+            module = self.module,
+            behaviors = self.behaviors,
+            rules = self.rules;
+
+        module.initDropdownModule();
+        module.initRatingModule();
+        module.initPopupModule();
+        module.initCheckboxModule();
+        module.initAccordionModule();
+
+        behaviors.closeDimmer();
+
+        rules.postalCode();
+    }
+}
+/**
  * Component responsible for handling different formats of the same product.
  *
  * @type {{productWithFormat: Function, productWithoutFormat: Function, updateBuybuttonAttributes: Function, updateProductInformation: Function, init: Function}}
@@ -2029,121 +2144,6 @@ var productQuantityContainer = {
 
     }
 };
-/**
- * Component responsible for activating semantic ui features.
- *
- * @type {{module: {initDropdownModule: Function, initRatingModule: Function, initPopupModule: Function, initCheckboxModule: Function}, behaviors: {closeDimmer: Function}, init: Function}}
- */
-var semanticInitContainer = {
-
-    /**
-     * Initialize modules
-     *
-     */
-    module: {
-        /**
-         * Initialize dropdown module.
-         *
-         */
-        initDropdownModule: function() {
-            $(".ui.dropdown").dropdown();
-
-            $(".ui.dropdown").on("click", function () {
-                var action = $(this).data("action") || "activate";
-
-                $(this).dropdown({
-                    action: action
-                });
-            });
-        },
-
-        /**
-         * Initialize rating module.
-         *
-         */
-        initRatingModule: function () {
-            $(".ui.rating").rating();
-        },
-
-        /**
-         * Initialize popup module.
-         *
-         */
-        initPopupModule: function () {
-            $(".popup").popup();
-        },
-
-        /**
-         * Initialize checkbox module.
-         *
-         */
-        initCheckboxModule: function () {
-            $('.ui.checkbox')
-                .checkbox()
-            ;
-        },
-
-        /**
-         * Initialize accordion module.
-         *
-         */
-        initAccordionModule: function() {
-            $('.ui.accordion').accordion();
-        }
-    },
-
-    /**
-     * Specify semantic custom behavior.
-     *
-     */
-    behaviors: {
-        closeDimmer: function () {
-            $(".close-dimmer").on("click", function() {
-                $(".dimmer").dimmer("hide");
-            });
-        }
-    },
-
-    /**
-     * Specify custom form validation rules.
-     *
-     */
-    rules: {
-        postalCode: function() {
-            $.fn.form.settings.rules.postalCode = function(value, fieldIdentifier) {
-                if(document.getElementById('checkboxSuccess').checked && fieldIdentifier == "billingCountry") {
-                    return true;
-                } else {
-                    if ($("#" + fieldIdentifier).val() === "CA")
-                        return value.match(/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} ?\d{1}[A-Z]{1}\d{1}$/i) ? true : false;
-                    else if ($("#" + fieldIdentifier).val() === "US")
-                        return value.match(/^\d{5}(?:[-\s]\d{4})?$/) ? true : false;
-                    else {
-                        return true;
-                    }
-                }
-            }
-        }
-    },
-
-
-    init: function () {
-        var self = semanticInitContainer,
-            module = self.module,
-            behaviors = self.behaviors,
-            rules = self.rules;
-
-        module.initDropdownModule();
-        module.initRatingModule();
-        module.initPopupModule();
-        module.initCheckboxModule();
-        module.initAccordionModule();
-
-        behaviors.closeDimmer();
-
-        rules.postalCode();
-    }
-}
 /**
  * Component responsible for the view component of each category page.
  *
