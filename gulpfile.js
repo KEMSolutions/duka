ee64c6d58a60695c7461de7aed42ca2da9af6440
Robert <nickdanvery@gmail.com>
2016-03-14 18:04:58 -0400

    Updated gulpfile, ran gulp --production to minify assets.

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,17 @@
  *******************************/
 
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     elixir = require('laravel-elixir');
 
-/*******************************
- Tasks
- *******************************/
 
-// Takes all scripts in public/js/dev and creates a minified, uglified production script.
-gulp.task('js', function () {
-    return gulp.src(
-        [
-            'public/js/dev/utils/*.js',
-            'public/js/dev/components/**/*.js',
-            'public/js/dev/init.js'
-        ])
-        .pipe(concat('duka.js'))
-        //.pipe(uglify())
-        .pipe(gulp.dest('public/js/prod'))
-});
-
-
-// Less compiler
 elixir(function(mix) {
+    // Compile less.
     mix.less('duka.less', "public/css/prod/duka.css");
+
+    // Concatenate scripts.
+    mix.scripts([
+        './public/js/dev/utils/*.js',
+        './public/js/dev/components/**/*.js',
+        './public/js/dev/init.js'
+    ], "public/js/prod/duka.js");
 });
