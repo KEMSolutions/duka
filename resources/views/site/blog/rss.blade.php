cbe1e3636b28474a95bd02e6e82e9942e0c2a55e
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2016-07-13 19:29:39 +0000

    Updated RSS/Atom to 2.0 specifications

--- resources/views/site/blog/rss.blade.php
+++ resources/views/site/blog/rss.blade.php
@@ -1,10 +1,12 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<feed xmlns="http://www.w3.org/2005/Atom">
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
     <title type="text">{{ Store::info()->name . " - " . Lang::get("boukem.blog") }}</title>
     <link href="http://duka.local:8000/fr/blog/rss"></link>
     <id>{{ action('BlogController@getFeed') }}</id>
     <language>{{ App::getLocale() }}</language>
-    <link rel="self" type="application/atom+xml" href="{{ action('BlogController@getFeed') }}" ></link>
+    <lastBuildDate>{{ Carbon\Carbon::now() }}</lastBuildDate>
+    <atom:link rel="self" type="application/atom+xml" href="{{ action('BlogController@getFeed') }}" />
     <icon>{{ url('/favicon.png') }}</icon>
         @if (count($blogs) > 0)
         <updated>{{ $blogs[0]->date }}</updated>
@@ -15,7 +17,7 @@
                 <name><![CDATA[{!! $blog->author->name !!}]]></name>
             </author>
             @if (isset($blog->image->url))
-            <enclosure length="1234" type="image/jpeg" url="{{ Utilities::setImageSizeAndMode(600, 600, 'fit', $blog->image->url) }}"/>
+            <enclosure length="1234" type="image/jpeg" url="{{ Utilities::setImageSizeAndMode(600, 600, 'fit', $blog->image->url) }}" />
             @endif
             <title type="text"><![CDATA[{!! $blog->title !!}}]]></title>
             <link rel="alternate" type="text/html" href="{{ URL::action('BlogController@show', ["slug"=>$blog->slug]) }}"></link>
@@ -25,4 +27,5 @@
             <updated>{{ $blog->date }}</updated>
         </entry>
         @endforeach
-</feed>
+    </channel>
+</rss>
