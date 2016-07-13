7126fc1c1ecb9959b807456e82dd3549dd9d7984
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2016-07-13 19:40:10 +0000

    Once again attempted to conform to RSS 2.0

--- resources/views/site/blog/rss.blade.php
+++ resources/views/site/blog/rss.blade.php
@@ -1,31 +1,23 @@
+
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-    <title type="text">{{ Store::info()->name . " - " . Lang::get("boukem.blog") }}</title>
-    <link href="http://duka.local:8000/fr/blog/rss"></link>
-    <id>{{ action('BlogController@getFeed') }}</id>
-    <language>{{ App::getLocale() }}</language>
-    <lastBuildDate>{{ Carbon\Carbon::now() }}</lastBuildDate>
-    <atom:link rel="self" type="application/atom+xml" href="{{ action('BlogController@getFeed') }}" />
-    <icon>{{ url('/favicon.png') }}</icon>
-        @if (count($blogs) > 0)
-        <updated>{{ $blogs[0]->date }}</updated>
-        @endif
-        @foreach ($blogs as $blog)
-        <entry>
-            <author>
-                <name><![CDATA[{!! $blog->author->name !!}]]></name>
-            </author>
-            @if (isset($blog->image->url))
-            <enclosure length="51385" type="image/jpeg" url="{{ Utilities::setImageSizeAndMode(600, 600, 'fit', $blog->image->url) }}" title="{!! $blog->title !!}"/>
-            @endif
-            <title type="text"><![CDATA[{!! $blog->title !!}}]]></title>
-            <link rel="alternate" type="text/html" href="{{ URL::action('BlogController@show', ["slug"=>$blog->slug]) }}"></link>
-            <id>{{ URL::action('BlogController@show', ["slug"=>$blog->slug]) }}</id>
-            <summary type="html"><![CDATA[{!! $blog->lead !!}]]></summary>
-            <content type="html"><![CDATA[{!!$parser->parse($blog->content)!!}]]></content>
-            <updated>{{ $blog->date }}</updated>
-        </entry>
-        @endforeach
-    </channel>
+ <channel>
+  <language>{{ App::getLocale() }}</language>
+  <title>{{ Store::info()->name . " - " . Lang::get("boukem.blog") }}</title>
+  <description></description>
+  <link>{{ url("/") }}</link>
+  <lastBuildDate>{{ Carbon\Carbon::now() }}</lastBuildDate>
+  <atom:link href="{{ url("/") }}" rel="self" type="application/rss+xml"/>
+  @foreach ($blogs as $blog)<item>
+   <title><![CDATA[{!! $blog->author->name !!}]]></title>
+   <enclosure length="256354" type="image/jpeg" url="{{ Utilities::setImageSizeAndMode(600, 600, 'fit', $blog->image->url) }}" title="{{ $blog->title }}"/>
+   <link><![CDATA[{{ URL::action('BlogController@show', ["slug"=>$blog->slug]) }}]]></link>
+   <guid isPermaLink="true">{{ URL::action('BlogController@show', ["slug"=>$blog->slug]) }}</guid>
+   <description><![CDATA[{!! $blog->lead !!}]]></description>
+   <pubDate>{{ $blog->date }}</pubDate>
+   <author>
+        <name><![CDATA[{{ $blog->author->name }}]]></name>
+    </author>
+  </item>@endforeach
+ </channel>
 </rss>
